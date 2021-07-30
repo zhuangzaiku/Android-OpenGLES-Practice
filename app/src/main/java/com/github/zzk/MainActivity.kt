fa189d5729ff6f4c0ba740aaaed48d2d7b0ae3f2
zaiku.zhuang <zaiku.zhuang@rokid.com>
2021-07-30 11:40:37 +0800

    [refactor] modify to kotlin

---
+++ app/src/main/java/com/github/zzk/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.zzk
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Author: Zack
+ * Date:   2021.07.30 11:38
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
